3881feac838f5bc01973191e2bd4e63c1e13dfdf
Pu Zhao <puzhao@AD.DOE.CARLETON.CA>
2017-07-21 17:14:42 -0400

    first commit

---
+++ array.js
@@ -0,0 +1,34 @@
+function isAvailable(array) {
+    return Boolean(array);
+}
+
+/**
+ * Returns the first element of an array. Passing n will return the first n elements of the array.
+ * @param {*} array 
+ * @param {*} n 
+ */
+function first(array, n = 1) {
+    if (isAvailable(array)) 
+        return Array.prototype.slice.call(array, 0, n);
+}
+
+/**
+ * Returns everything but the last entry of the array. Especially useful on the arguments object. Pass n to exclude the last n elements from the result.
+ * @param {*} array 
+ * @param {*} n 
+ */
+function initial(array, n = 1) {
+    if (isAvailable(array)) 
+        return Array.prototype.slice.call(array, 0, array.length - n);
+}
+
+/**
+ * Returns the last element of an array. Passing n will return the last n elements of the array.
+ * @param {*} array 
+ * @param {*} n 
+ */
+function last(array, n = 1) {
+    if (isAvailable(array))
+        return Array.prototype.slice.call(array, array.length - n);
+}
+
